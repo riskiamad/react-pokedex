f1a17fb6e8ae4285fa82f988a1d36eb949922fde
Achmad Rizky Syahrani <achmad.rizky@kodingworks.io>
2022-09-07 07:40:24 +0700

    upd: add new route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Container } from 'react-bootstrap';
 // Pages
 import Homepage from './pages/Homepage';
 import PokemonPage from './pages/PokemonPage';
+import MyPokemonPage from './pages/MyPokemonPage';
+import DetailMyPokemonPage from './pages/DetailMyPokemonPage';
 
 // Components
 import Header from './components/Header';
@@ -16,6 +18,8 @@ const App = () => {
       <Container>
         <Route exact path='/' component={Homepage} />
         <Route path='/pokemon/:id' component={PokemonPage}/>
+        <Route exact path='/mypokemon' component={MyPokemonPage}/>
+        <Route path='/mypokemon/:id' component={DetailMyPokemonPage}/>
       </Container>
     </Router>
   );
